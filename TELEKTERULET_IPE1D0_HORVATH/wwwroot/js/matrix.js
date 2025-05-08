8f8600f93988aa5d059a580564700b31d163160d
unknown <bandi200306151@gmail.com>
2025-05-08 22:53:08 +0200

    Basic Javascript setup

--- TELEKTERULET_IPE1D0_HORVATH/wwwroot/js/matrix.js
+++ TELEKTERULET_IPE1D0_HORVATH/wwwroot/js/matrix.js
@@ -1,7 +1,30 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
+    const matrixinput = document.getElementById("matrixinput");
+    const epsziloninput = document.getElementById("epsziloninput");
+    const calculatebutton = document.getElementById("calculatebutton");
+    const resultarea = document.getElementById("resultarea");
+    const matrixvisualization = document.getElementById("matrixvisualization");
+    const apiurl = "api/landarea/calculate";
 
+    function parseMatrixInput(input) {
+        
+    }
 
+    function isValidMatrix(matrix) {
+        
+    }
 
+    async function SendtoBackend(matrix, epszilon) {
+        
+    }
+
+    function displayResult(result) {
+        
+    }
+
+    function displayMatrix(matrix, largestareaindexes) {
+
+    }
 
 
 
